399b8f9e94f4b399732602c2a9bb66e12706c319
Riley Treat <145404426+RileyTreat@users.noreply.github.com>
2025-09-18 16:17:05 -0400

    Time: 0 ms (100%), Space: 53.8 MB (32.81%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n < 2) {
+        return 1;
+    }
+
+    let firstStep = 1;
+    let secondStep = 1;
+    let thridStep = 0;
+
+    for (let i = 2; i <= n; i++){
+        thirdStep = firstStep + secondStep;
+
+        firstStep = secondStep;
+
+        secondStep = thirdStep;
+    }
+    return thirdStep;
+}
